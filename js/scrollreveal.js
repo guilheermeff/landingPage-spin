d47cc3a0f753af1412ea610fee6aaef5d246c5af
Guilherme Fernandes <guilheermeff@outlook.com>
2022-08-29 12:14:43 -0400

    update reveal

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,7 +1,7 @@
 export function scroll() {
   window.sr = ScrollReveal({reset: true})
 
-  ScrollReveal().reveal('header',{
+  ScrollReveal().reveal('.logo',{
     delay: 100,
     rotate: {
       x: 100,
@@ -9,8 +9,31 @@ export function scroll() {
       z: 0
     }
   })
-
-  ScrollReveal().reveal('.hello',{
+  ScrollReveal().reveal('.nav-bar',{
+    delay: 100,
+    rotate: {
+      x: 100,
+      y: 0,
+      z: 0
+    }
+  })
+  ScrollReveal().reveal('.textBox',{
+    delay: 100,
+    rotate: {
+      x: 100,
+      y: 0,
+      z: 0
+    }
+  })
+  ScrollReveal().reveal('.btnWhatsapp',{
+    delay: 100,
+    rotate: {
+      x: 100,
+      y: 0,
+      z: 0
+    }
+  })
+  ScrollReveal().reveal('.gradient',{
     delay: 100,
     rotate: {
       x: 100,
